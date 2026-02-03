8deeac27538850547bb3a187e8c15b92bbcc444a
Chirag <chhirag920@gmail.com>
2026-02-03 16:04:08 +0530

    call to action section

--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -39,10 +39,10 @@ export default function Faqs() {
         <div className="flex justify-center">
           <Tag>Faqs</Tag>
         </div>
-        <h2 className="text-6xl font-medium mt-6 text-center">
+        <h2 className="text-6xl font-medium mt-6 text-center max-w-xl mx-auto">
           Questions? We've got <span className="text-lime-400">answers</span>
         </h2>
-        <div className="mt-12 flex flex-col gap-6">
+        <div className="mt-12 flex flex-col gap-6 max-w-xl mx-auto">
           {faqs.map((faq, faqIndex) => (
             <div
               key={faq.question}
